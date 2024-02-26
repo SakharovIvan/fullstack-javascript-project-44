b00efb099f83ec0adf14e8d1e0c63543f9263214
i.sakharov <i731901@gmail.com>
2024-02-26 17:42:00 +0300

    Replay games mode

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -7,27 +7,43 @@ import progression from '../bin/brain-progression.js'
 import prime from '../bin/brain-prime.js'
 import readlineSync from 'readline-sync';
 
+const gameSwitch= (game,client)=>{
+    switch (game) {
+        case ('even-game'):
+            evenGame(client)
+            break
+        case ('calc-game'):
+            calcGame(client)
+        break
+        case ('node-game'):
+            nodeGame(client)
+            break
+        case ('progression'):
+            progression(client)
+            break
+        case ('prime'):
+            prime(client)
+            break
+        default: break
+    }
+    }
+
+const replay= (replayAnswer)=>{
+    replayAnswer = readlineSync.question("Do you want replay?  ");
+    if (replayAnswer==="yes"){
+    console.log("Select your game \n1) even-game \n2) calc-game \n3) node-game \n4) progression \n5) prime" )
+    const game = readlineSync.question("Your selection: ");
+    gameSwitch(game,client)
+    replay()
+}else{return}
+
+}
+    
 console.log("Welcome to the Brain Games!")
 const client = name();
 console.log ("Hello, "+client+"!")
 console.log("Select your game \n1) even-game \n2) calc-game \n3) node-game \n4) progression \n5) prime" )
 const game = readlineSync.question("Your selection: ");
-switch (game) {
-    case ('even-game'):
-        evenGame(client)
-        break
-    case ('calc-game'):
-        calcGame(client)
-    break
-    case ('node-game'):
-        nodeGame(client)
-        break
-    case ('progression'):
-        progression(client)
-        break
-    case ('prime'):
-        prime(client)
-        break
-    default: break
-}
+gameSwitch(game,client)
+replay() 
 
