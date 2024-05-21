fb472a05e4753c317e03c55de292a2ca58b97141
i.sakharov <i731901@gmail.com>
2024-05-21 13:32:46 +0300

    CRLF -> LF 14

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,4 @@
-export default getRandomInRange = (min = 0, max = 100) =>
+const getRandomInRange = (min = 0, max = 100) =>
   Math.floor(min + Math.random() * (max - min + 1));
+
+export default getRandomInRange;
